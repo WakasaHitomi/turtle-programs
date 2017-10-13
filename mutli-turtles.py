ab4ca885d841fc13d7c9554a8bb86536bd32ee02
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-10-13 09:31:34 -0400

    Add files via upload

--- mutli-turtles.py
+++ mutli-turtles.py
@@ -1,33 +1,45 @@
 import random
 from turtle import *
 
+# tkinter colors
+
 Wakasa = Turtle()
 Momo = Turtle()
 Kassie = Turtle()
+Peter = Turtle()
+Lambert = Turtle()
 
 Wakasa.shape("turtle")
-Wakasa.color("purple")
+Wakasa.color("DarkOrchid1")
 Wakasa.shapesize(5)
 Wakasa.pensize(6)
 Wakasa.speed(10)
 
 
 Momo.shape("turtle")
-Momo.color("blue")
+Momo.color("turquoise")
 Momo.shapesize(5)
 Momo.pensize(10)
+Momo.speed(10)
 
 Kassie.shape("turtle")
-Kassie.color("black")
+Kassie.color("slate gray")
 Kassie.shapesize(5)
 Kassie.pensize(8)
+Kassie.speed(10)
 
 for i in range(25):
-    while True:
-        Kassie.fd(200)
-        Kassie.rt(30)
-        Momo.lt(80)
-        Wakasa.lt(5)
-        Wakasa.fd(20)
-        Momo.fd(180)
-        Kassie.fd(90)
+    Kassie.fd(50)
+    Kassie.rt(50)
+    Momo.lt(80)
+    Wakasa.lt(10)
+    Wakasa.fd(40)
+    Momo.fd(180)
+    Peter.fd(90)
+    Lambert.lt(9)
+    Peter.fd(60)
+    Lambert.fd(88)
+
+
+
+
