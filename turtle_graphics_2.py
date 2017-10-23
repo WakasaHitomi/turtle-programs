15336b923a33b86a81425cf3aff4153cb57b6b70
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-10-23 09:11:49 -0400

    Add files via upload

--- turtle_graphics_2.py
+++ turtle_graphics_2.py
@@ -6,6 +6,22 @@ Kassie = Turtle()
 Wakasa = Turtle()
 Saizo = Turtle()
 
+def luna_Moon(x, y, points, line, fill):
+    penup()
+    goto(x, y)
+    pendown()
+
+    color(line, fill)
+
+    begin_fill()
+    for i in range(points):
+        lt(10)
+        fd(40)
+    end_fill()
+
+speed(10)
+
+luna_moon(300, -200, 0, "black", "azure3"
 def Luna_Moon(x, y, points, line, fill):
     penup()
     goto(x, y)
@@ -15,16 +31,7 @@ def Luna_Moon(x, y, points, line, fill):
 
     begin_fill()
     for i in range(points):
-        
-def Luna_Moon(x, y, points, line, fill):
-    penup()
-    goto(x, y)
-    pendown()
-
-    color(line, fill)
-
-    begin_fill()
-    for i in range(points):
+    pass
 
 def Luna_Moon(x, y, points, line, fill):
     penup()
@@ -35,6 +42,7 @@ def Luna_Moon(x, y, points, line, fill):
 
     begin_fill()
     for i in range(points):
+    pass
 
 def Luna_Moon(x, y, points, line, fill):
     penup()
@@ -45,3 +53,4 @@ def Luna_Moon(x, y, points, line, fill):
 
     begin_fill()
     for i in range(points):
+    pass
