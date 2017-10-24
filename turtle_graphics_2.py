1820ee4421c574dd2ed16c6390c9ffaa7a0875f7
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-10-24 00:19:14 -0400

    Add files via upload

--- turtle_graphics_2.py
+++ turtle_graphics_2.py
@@ -16,15 +16,55 @@ def luna_moon(x, y, points, line, fill):
     color(line, fill)
 
     begin_fill()
-    for i in range(25):
-            lt(10)
-            fd(40)
-        if abs(pos()) < 1:
-            break
+    for i in range(72):
+            lt(5)
+            fd(20)
     end_fill()
-    done()
+
 
 speed(10)
 
-luna_moon(300, -200, 1, "black", "azure3")
+luna_moon(-400, -50, 1, "black", "azure3")
+luna_moon(400, -50, 1, "black", "azure3")
 
+def small_dots(x, y, points, line, fill):
+    penup()
+    goto(x, y)
+    pendown()
+
+    turn = 180 - (360 / points)
+
+    color(line, fill)
+
+    begin_fill()
+    for i in range(100):
+            lt(15)
+            fd(10)
+    end_fill()
+
+
+speed(10)
+
+
+small_dots(-420, -100, 1, "LightBlue1", "light sky blue")
+
+def eye_pupil(x, y, points, line, fill):
+    penup()
+    goto(x, y)
+    pendown()
+
+    turn = 180 - (360 / points)
+
+    color(line, fill)
+
+    begin_fill()
+    for i in range(100):
+            lt(10)
+            fd(15)
+    end_fill()
+
+speed(10)
+
+
+eye_pupil(-370, -40, 1, "cornsilk4", "honeydew4")
+eye_pupil(170, 40, 1, "cornsilk4", "honeydew4")
